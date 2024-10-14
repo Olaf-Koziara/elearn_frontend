e5bb685e755b341b99fd7f219aa090a2c6a70519
Olaf1303 <olaf.koziara@gmail.com>
2024-10-14 10:40:21 +0200

    add function Style depends on button type

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -10,20 +10,21 @@ type ButtonProps = {
 };
 
 // Definicja stylowanego komponentu Button
-const StyledButton = styled.button<{ disabled: boolean }>`
-    padding: 10px 20px;
-    background-color: ${({ disabled }) => (disabled ? '#ccc' : '#007bff')};
-    color: white;
-    border: none;
-    border-radius: 4px;
-    font-size: 16px;
-    cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
-    opacity: ${({ disabled }) => (disabled ? 0.6 : 1)};
-    transition: background-color 0.3s ease;
+const StyledButton = styled.button<{ disabled: boolean, type: string }>`
+  padding: 10px 20px;
+  background-color: ${({disabled}) => (disabled ? '#ccc' : '#007bff')};
+  color: white;
+  border: none;
+  border-radius: 4px;
+  font-weight: ${({type}) => (type === 'button' ? '500' : 'bold')};
+  font-size: 16px;
+  cursor: ${({disabled}) => (disabled ? 'not-allowed' : 'pointer')};
+  opacity: ${({disabled}) => (disabled ? 0.6 : 1)};
+  transition: background-color 0.3s ease;
 
-    &:hover {
-        background-color: ${({ disabled }) => (disabled ? '#ccc' : '#0056b3')};
-    }
+  &:hover {
+    background-color: ${({disabled}) => (disabled ? '#ccc' : '#0056b3')};
+  }
 `;
 
 const Button: React.FC<ButtonProps> = ({
