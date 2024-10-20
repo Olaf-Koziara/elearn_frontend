a5122dece847aa511da4bb92f0b2d2f2e0b57740
Olaf1303 <olaf.koziara@gmail.com>
2024-10-20 17:22:38 +0200

    auth update

--- src/components/FormField/FormField.tsx
+++ src/components/FormField/FormField.tsx
@@ -18,50 +18,50 @@ type FormFieldProps = {
 const FormField: React.FC<FormFieldProps> = ({
                                                  label,
                                                  type = 'text', // Domyślnie "text"
-                                                 value,
-                                                 onChange,
-                                                 onInput,
                                                  placeholder,
                                                  name,
                                                  required = false,
                                                  className,
                                                  disabled = false,
+                                                 ...otherProps
                                              }) => {
-    const FormFieldWrapper = styled.div<{ $disabled?: boolean }>`
-      padding: 0.4rem 0.8rem;
-      position: relative;
-      background-image: ${(props) => props.$disabled ? 'linear-gradient(rgba(230, 230, 230, 0.3),rgba(230, 230, 230, 0.3))' : `linear-gradient(#20aee3, #20aee3), linear-gradient(#bfbfbf, #bfbfbf)`};
-      background-size: ${(props) => props.$disabled ? '100% 100%' : '0 2px, 0 1px'};
-      background-position: ${(props) => props.$disabled ? 'left bottom' : 'center bottom, center calc(100% - 1px)'};
-      background-repeat: no-repeat;
-      text-shadow: 0 2px 3px rgba(0, 0, 0, 0.1);
-      box-shadow: 0 1px 3px rgba(0, 0, 0, 0.15) inset;
-      border-radius: 20px;
-      transition: background-size 200ms linear;
 
-
-      &:focus-within {
-        background-size: 100% 1px, 100% 1px;
-      }
-
-    `;
-    const FormField = styled.input<{ $disabled?: boolean, $outline?: boolean }>`
-      background: transparent;
-      width: 100%;
-      outline: none;
-      cursor: ${(props) => props.$disabled ? 'default' : 'auto'};
-      pointer-events: ${(props) => props.$disabled ? 'none' : 'all'};
-      border: none`
 
     return (
         <FormFieldWrapper className={`form-field ${className}`} $disabled={disabled}>
             {label && <label htmlFor={name}>{label}</label>}
-            <FormField type={type} value={value} onChange={onChange} onInput={onInput} required={required}
-                       $disabled={disabled}>
+            <FormFieldStyled type={type}
+                             required={required}
+                             $disabled={disabled}
+                             {...otherProps}>
 
-            </FormField>
+            </FormFieldStyled>
         </FormFieldWrapper>
     );
 };
+const FormFieldStyled = styled.input<{ $disabled?: boolean, $outline?: boolean }>`
+  background: transparent;
+  width: 100%;
+  outline: none;
+  cursor: ${(props) => props.$disabled ? 'default' : 'auto'};
+  pointer-events: ${(props) => props.$disabled ? 'none' : 'all'};
+  border: none`
+const FormFieldWrapper = styled.div<{ $disabled?: boolean }>`
+  padding: 0.4rem 0.8rem;
+  position: relative;
+  background-image: ${(props) => props.$disabled ? 'linear-gradient(rgba(230, 230, 230, 0.3),rgba(230, 230, 230, 0.3))' : `linear-gradient(#20aee3, #20aee3), linear-gradient(#bfbfbf, #bfbfbf)`};
+  background-size: ${(props) => props.$disabled ? '100% 100%' : '0 2px, 0 1px'};
+  background-position: ${(props) => props.$disabled ? 'left bottom' : 'center bottom, center calc(100% - 1px)'};
+  background-repeat: no-repeat;
+  text-shadow: 0 2px 3px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.15) inset;
+  border-radius: 20px;
+  transition: background-size 200ms linear;
 
+
+  &:focus-within {
+    background-size: 100% 1px, 100% 1px;
+  }
+
+`;
 export default FormField;
--- src/features/auth/AuthForm.tsx
+++ src/features/auth/AuthForm.tsx
@@ -1,7 +1,7 @@
-import React, {FormEvent, useState} from 'react';
+import React, {ChangeEvent, FormEvent, useCallback, useState} from 'react';
 import FormField from "../../components/FormField/FormField";
 import Button from "../../components/Button/Button";
-import {login, register} from "../../services/authService";
+import {useAuth} from "../../hooks/useAuth";
 
 
 type AuthFormProps = {
@@ -9,67 +9,78 @@ type AuthFormProps = {
 
 };
 
-const AuthForm: React.FC<AuthFormProps> = ({ type }) => {
+const AuthForm: React.FC<AuthFormProps> = ({type}) => {
     const [email, setEmail] = useState<string>('olk@wp.pl');
     const [password, setPassword] = useState<string>('12olaf34');
     const [confirmPassword, setConfirmPassword] = useState<string>('12olaf34');
     const [name, setName] = useState<string>('olaf');
     const [surname, setSurname] = useState<string>('koziara')
+    const {login, register} = useAuth();
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
         // Jeśli jest to rejestracja, przekazujemy confirmPassword, w przeciwnym razie tylko email i password
-        if(type === "login"){
-            login(email,password)
-        }else{
-            register(email,password,confirmPassword,name,surname)
+        if (type === "login") {
+            login(email, password).then(result => {
+                console.log(result)
+            }).catch(err => {
+                console.log(err)
+            })
+        } else {
+            register(email, password, confirmPassword, name, surname)
         }
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <h2>{type === 'login' ? 'Login' : 'Register'}</h2>
+        <div className="row justify-content-center mt-5">
+            <div className="col-md-3 text-center">
+                <h2>{type === 'login' ? 'Login' : 'Register'}</h2>
+                <form onSubmit={handleSubmit} className="row card pt-4 ">
 
-            <FormField
-                label="Email"
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Enter your email"
-                required
-            />
+                    <div className="col-md-12 pb-3">
+                        <FormField
+                            type="email"
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+                            value={email}
+                            placeholder="Enter your email"
+                            required
+                        />
+                    </div>
+                    <div className='col-md-12 pb-4'>
 
-            <FormField
-                label="Password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Enter your password"
-                required
-            />
+                        <FormField
+                            type="password"
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+                            value={password}
+                            placeholder="Enter your password"
+                            required
+                        />
+                    </div>
 
-            {type === 'register' && (
-                <>
-                <FormField
-                    label="Confirm Password"
-                    type="password"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                    placeholder="Confirm your password"
-                    required
-                />
-                <FormField label="Name" value={name} onChange={(e)=>setName(e.target.value)}/><FormField label="Surname" value={surname} onChange={(e)=>setSurname(e.target.value)}/>
+                    {type === 'register' && (
+                        <>
+                            <FormField
+                                label="Confirm Password"
+                                type="password"
+                                value={confirmPassword}
+                                placeholder="Confirm your password"
+                                required
+                            />
+                            <FormField label="Name" value={name} onChange={(e) => setName(e.target.value)}/><FormField
+                            label="Surname" value={surname} onChange={(e) => setSurname(e.target.value)}/>
 
 
-                </>
-            )}
+                        </>
+                    )}
 
-            <Button
-                label={type === 'login' ? 'Login' : 'Register'}
-                type="submit"
-                className="auth-button"
-            />
-        </form>
+                    <Button
+                        label={type === 'login' ? 'Login' : 'Register'}
+                        type="submit"
+                        className="auth-button"
+                    />
+                </form>
+            </div>
+        </div>
     );
 };
 
