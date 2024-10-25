8c1996a7225d26747d7341b3931396aed7c56454
Olaf1303 <olaf.koziara@gmail.com>
2024-10-25 18:31:54 +0200

    auth update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -21,22 +21,23 @@ const Header = () => {
     }, [data, dispatch])
 
     return (
-        <header className="navbar navbar-expand-lg navbar-light bg-light">
+        <header className="header navbar navbar-expand-lg navbar-light bg-light">
 
-            <nav className='container navigation navbar-nav justify-content-end'>
-                <NavLink className="nav-link" to='/'>Home</NavLink>
+            <nav className='container navigation navbar-nav justify-content-between'>
+                <NavLink className="nav-link home-link" to='/'><i
+                    className="bi bi-highlighter"></i>Dashboard</NavLink>
                 {!userInfo && <>
                     <NavLink className="nav-link" to='/register'>Register</NavLink>
                 </>
                 }
                 <div className='header-user'>
-                    <i className='header-user_icon bi bi-person-circle' style={{fontSize: '1.5rem'}}
+                    <i className='header-user_icon bi bi-person-circle'
                        onClick={() => setIsUserInfoClicked(!isUserInfoClicked)}
                     ></i>
                     <div className={`header-user_dropdown ${isUserInfoClicked ? 'open' : ''} card p-4`}>
                         {userInfo ? (
                             <>
-                                <span>{userInfo.email}</span>
+                                <span className="pb-2">{userInfo.email}</span>
                                 <NavLink className="btn btn-outline-primary mb-2" to='/user-profile'>Profile</NavLink>
                                 <button className='button btn btn-primary' onClick={() => dispatch(logout())}>
                                     Logout
